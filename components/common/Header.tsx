034c153dc0206e8f6a77f59ee8aae67146807802
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2024-01-03 12:52:50 -0300

    feat: Creates Header component

---
+++ components/common/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='text-white font-semibold text-2xl '>Gabriel Rodrigues</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
